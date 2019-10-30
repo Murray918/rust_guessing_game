56ca0bb397a2741e1b62e193e39f39d99a90812d
Andrew Murray <darbyguitars@gmail.com>
2019-10-30 14:13:34 -0500

    creating the main function of our guessing game

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use std::io // brings in the standard input/output module
+
+// this is the main function
+fn main() {
+    
+    println!("Guess the number");
+
+    println!("Please input your guess.");
+
+    // create a new mutable variable of type string
+    let mut guess = String::new()
+
+    /* 
+    use the standard library to call read line on the line inputted by user
+    the & indicates that the argument is a reference to var in memory
+    the .expect() lets us handle errors
+     */
+    is::stdin().read_line(&mut guess)
+        .expect("Failed to Read line");
+
+     println!("You guessed: {}", guess);
+}
