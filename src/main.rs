6c5aa5a426a7db5a3a06c6bf3f51c8459e42c6b4
Andrew Murray <darbyguitars@gmail.com>
2019-10-30 14:24:15 -0500

    syntax errors on run fixed

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io // brings in the standard input/output module
+use std::io; // brings in the standard input/output module
 
 // this is the main function
 fn main() {
@@ -8,14 +8,14 @@ fn main() {
     println!("Please input your guess.");
 
     // create a new mutable variable of type string
-    let mut guess = String::new()
+    let mut guess = String::new();
 
     /* 
     use the standard library to call read line on the line inputted by user
     the & indicates that the argument is a reference to var in memory
     the .expect() lets us handle errors
      */
-    is::stdin().read_line(&mut guess)
+    io::stdin().read_line(&mut guess)
         .expect("Failed to Read line");
 
      println!("You guessed: {}", guess);
